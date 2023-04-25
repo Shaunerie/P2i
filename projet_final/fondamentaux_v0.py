d60549c1073605d54974be9d6abcf04a3539b901
Shaunerie <103446961+Shaunerie@users.noreply.github.com>
2023-04-25 12:57:52 +0200

    v0.1 projet final fondamentaux

--- projet_final/fondamentaux_v0.py
+++ projet_final/fondamentaux_v0.py
@@ -24,20 +24,32 @@ def calcul_vitesse(vitesse, acceleration, pas_de_temps):
     """
     return vitesse + acceleration * pas_de_temps/2
 
-def calcul_allongement(vitesse_i, vitesse_j, position_i, position_j):
+def calcul_disance_normal(vitesse_i, vitesse_j, position_i, position_j, rayon_i, rayon_j):
     """
-    Calcul de l'allongement des ressorts tangentiels à partir de l'équation (4.8)
-    in: vitesse_i, vitesse_j, position_i, position_j (tableaux numpy)
-    out: allongement (tableau numpy)
+    Calcul de l'allongement/distance normal à partir de l'équation
+    in: vitesse_i, vitesse_j, position_i, position_j (tableaux numpy), rayon_i, rayon_j (float)
+    out: distance/allongement normal (float)
     """
-    #Raideur tangentielle = allongement
-    #(4.8): d\delta/dt = (vitesse_i - vitesse_j)*tangente 
+    return np.linalg.norm(position_i - position_j) - (rayon_i + rayon_j)
+
+def calcul_allongement_tangentiel(vitesse_i, vitesse_j, position_i, position_j, rayon_i, rayon_j):
+    """
+    Calcul de l'allongement tangentielle à partir de l'équation
+    in: vitesse_i, vitesse_j, position_i, position_j (tableaux numpy), rayon_i, rayon_j (float)
+    out: allongement tangentielle (float)
+    """
+    #(4.8): d/dt(delta_t) = (vitesse_i - vitesse_j)*tangente 
     # C'est le produit vectorielle de la différence de vitesse sur la tangente du contact entre les deux grains i et j
     #C'est cette formule (4.8) qu'il faut intégrer temporellement en utilisant les données des vitesses(tableaux numpy)
-    tangente = ?
-    derivee = ?
-    integration = ?
-    return integration
+    #On calcul ensuite l'allongement tangentielle via la formule (4.8)
+    vecteur_normal = (position_i - position_j)/np.linalg.norm(position_i - position_j)
+    vitesse_relative = vitesse_i - vitesse_j
+    vitesse_tangentielle = vitesse_relative - np.dot(vitesse_relative, vecteur_normal)*vecteur_normal
+    #On doit maintenant intégrer la valeur de la vitesse tangentielle sur le temps
+    #En fait c pas facile parce qu'on doit utiliser le cumsum avec le pas de temps mais a partir 
+    #d'un temps précis, celui du contact entre les deux grains... ou ca correspond au repos du ressort
+    #tangentielle (allongement_tangetielle = 0)
+
 
 def application_efforts_distance(masse):
     """
@@ -57,60 +69,73 @@ def mise_a_jour_resultante(grain, force_contact):
     # Remplacez cette ligne par la mise à jour de la résultante des forces sur grain
     pass
 
-# 1. Définition de l'état initial pour chaque particule composant l'échantillon
-# (À adapter en fonction de votre situation)
-etat_initial = []
 
-# 2. Définition de la valeur du pas de temps
-pas_de_temps = 0.01
+if __name__ == "__main__":
+    # 0. Définition des paramètres physiques du problème
+    masse = 1e-3 #kg
+    rayon = 1e-2 #m
+    raideur_normale = 1 #N/m
+    raideur_tangentielle = 1 #N/m
+    coefficient_de_frottement = 0.5 #N/m
+    nb_grains = 1000
+    # 2. Définition de la valeur du pas de temps
+    pas_de_temps = 0.01
+    # 3. Définition de la durée souhaitée de la simulation
+    duree_simulation = 10
+    # 4. Initialisation du temps
+    temps = 0
+    #Pour le roulement trop compliqué, on utilise leur rotation
+    nb_temps = duree_simulation//pas_de_temps
+    #Définition du silo
+    hauteur_silo = 1 #m
+    largeur_silo = 1 #m
 
-# 3. Définition de la durée souhaitée de la simulation
-duree_simulation = 10
+    # 1. Définition de l'état initial pour chaque particule composant l'échantillon
 
-# 4. Initialisation du temps
-temps = 0
 
-# 5. Tant que temps < duree_simulation, boucle principale
-while temps < duree_simulation:
-    # 6. Avancée dans le temps
-    temps += pas_de_temps
-    
-    # 7. Boucle sur tous les grains
-    for grain in etat_initial:
-        # 8. Calcul de la nouvelle position du grain
-        calcul_nouvelle_position(grain)
-        
-        # 9. Calcul de la vitesse du grain
-        calcul_vitesse(grain)
-        
-        # 10. Mise à jour de l'allongement des ressorts tangentiels
-        calcul_allongement(grain)
-    
-    # 12. Boucle sur tous les grains
-    for grain1 in etat_initial:
-        # 13. Initialisation des résultantes d'efforts sur le grain
-        force_resultante = np.zeros(3)
-        
-        # 14. Application des efforts à distance (par exemple la pesanteur)
-        application_efforts_distance(grain1)
-        
-        # 15. Boucle sur tous les grains
-        for grain2 in etat_initial:
-            if grain1 != grain2:
-                # 16. En cas de contact entre grain1 et grain2, calcul de l'effort de contact
-                force_contact = calcul_effort_contact(grain1, grain2)
-                
-                # 17. Mise à jour de la résultante des forces sur grain1
-                mise_a_jour_resultante(grain1, force_contact)
-        
-        # 19. Calcul de l'accélération du grain à partir de l'équation (4.1)
-        # (Remplacez cette ligne par le calcul de l'accélération à partir de l'équation (4.1))
-        
-        # 20. Calcul de la vitesse de demi-pas de temps à partir de l'équation (4.19)
-        # (Remplacez cette ligne par le calcul de la vitesse de demi-pas à partir de l'équation (4.19))
 
-    # 22. Sauvegarde, Tracé, Monitoring, etc.
-    # (Ajoutez ici le code pour sauvegarder, tracer et/ou surveiller les données de la simulation)
 
-# 23. Fin de la boucle principale
+    # 5. Tant que temps < duree_simulation, boucle principale
+    while temps < duree_simulation:
+        # 6. Avancée dans le temps
+        temps += pas_de_temps
+        
+        # 7. Boucle sur tous les grains
+        for grain in etat_initial:
+            # 8. Calcul de la nouvelle position du grain
+            calcul_nouvelle_position(grain)
+            
+            # 9. Calcul de la vitesse du grain
+            calcul_vitesse(grain)
+            
+            # 10. Mise à jour de l'allongement des ressorts tangentiels
+            calcul_allongement(grain)
+        
+        # 12. Boucle sur tous les grains
+        for grain1 in etat_initial:
+            # 13. Initialisation des résultantes d'efforts sur le grain
+            force_resultante = np.zeros(3)
+            
+            # 14. Application des efforts à distance (par exemple la pesanteur)
+            application_efforts_distance(grain1)
+            
+            # 15. Boucle sur tous les grains
+            for grain2 in etat_initial:
+                if grain1 != grain2:
+                    # 16. En cas de contact entre grain1 et grain2, calcul de l'effort de contact
+                    force_contact = calcul_effort_contact(grain1, grain2)
+                    
+                    # 17. Mise à jour de la résultante des forces sur grain1
+                    mise_a_jour_resultante(grain1, force_contact)
+            
+            # 19. Calcul de l'accélération du grain à partir de l'équation (4.1)
+            # (Remplacez cette ligne par le calcul de l'accélération à partir de l'équation (4.1))
+            
+            # 20. Calcul de la vitesse de demi-pas de temps à partir de l'équation (4.19)
+            # (Remplacez cette ligne par le calcul de la vitesse de demi-pas à partir de l'équation (4.19))
+
+        # 22. Sauvegarde, Tracé, Monitoring, etc.
+        # (Ajoutez ici le code pour sauvegarder, tracer et/ou surveiller les données de la simulation)
+
+    # 23. Fin de la boucle principale
 
